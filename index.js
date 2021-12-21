fcb79bac8952108b444bbaf144098084599c3aef
macflower <stonemm21074mm@gmail.com>
2021-12-21 17:37:49 +0500

    commit Context "this"

--- index.js
+++ index.js
@@ -1,16 +1,48 @@
-const person = new Object({
-    name: 'Maxim',
-    age: 25,
-    greet: function () {
-        console.log('Greet!')
-    }
-})
-
-Object.prototype.sayHello = function () {
-    console.log('Hello!')
+function hello() {
+    console.log('Hello', this)
 }
 
-const Lena = Object.create(person)
-Lena.name = 'Elena'
+const person = {
+    name: 'Vladilen',
+    age: 25,
+    sayHello: hello,
+    sayHelloWindow: hello.bind(document),
+    logInfo: function(job, phone) {
+        console.group(`${this.name} info:`)
+        console.log(`Name is ${this.name}`)
+        console.log(`Age is ${this.age}`)
+        console.log(`Job is ${job}`)
+        console.log(`Phone is ${phone}`)
+        console.groupEnd()
+    }
+}
 
-const str = new String('I am string')
+const lena = {
+    name: 'Elena',
+    age: 23
+}
+
+// person.logInfo.bind(lena, 'Frontend', '8-999-123-12-23')()
+// person.logInfo.call(lena, 'Frontend', '8-999-123-12-23')
+person.logInfo.apply(lena, ['Frontend', '8-999-123-12-23'])
+
+/// ===============
+
+const array = [1, 2, 3, 4, 5]
+
+// function multBy(arr, n) {
+//     return arr.map(function (i) {
+//         return i * n
+//     })
+// }
+
+Array.prototype.multBy = function (n) {
+    return this.map(function (i) {
+        return i * n
+    })
+}
+
+console.log(array.multBy(20))
+
+
+// console.log(multBy(array, 15))
