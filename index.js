ccf9a32d0e0ffdea99b51b07791f4eaa951d42b1
macflower <stonemm21074mm@gmail.com>
2021-12-30 18:33:23 +0500

    commit Promises

--- index.js
+++ index.js
@@ -1,15 +1,69 @@
-console.log('Start')
+console.log('Request data...')
 
-console.log('Start 2')
+// setTimeout( () => {
+//     console.log('Preparing data...')
+//
+//     const backendData = {
+//         server: 'aws',
+//         port: 2000,
+//         status: 'working'
+//     }
+//
+//     setTimeout(() => {
+//         backendData.modified = true,
+//         console.log('Data received', backendData)
+//     }, 2000)
+// }, 2000)
 
-function timeout2sec () {
-    console.log('timeout2sec')
+// const p = new Promise(function (resolve, reject) {
+//     setTimeout(() => {
+//         console.log('Preparing data...')
+//         const backendData = {
+//         server: 'aws',
+//         port: 2000,
+//         status: 'working'
+//         }
+//         resolve(backendData)
+//     }, 2000)
+// })
+//
+// p.then(data => {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//         data.modified = true,
+//             resolve(data)
+//         }, 2000)
+//     })
+    // console.log('Promise resolved',data)
+
+    // p2.then(clientData => {
+    //     console.log('Data received', clientData)
+    // })
+// })
+//     .then(clientData => {
+//     console.log('Data received', clientData)
+//     clientData.fromPromise = true
+//     return clientData
+// })
+//     .then(data => {
+//     console.log('Modified', data)
+// })
+//     .catch(err => console.error('Error: ', err))
+//     .finally(() => console.log('Finally'))
+
+const sleep = ms => {
+    return new Promise(resolve => {
+        setTimeout(() => resolve(), ms)
+    })
 }
 
-window.setTimeout(function () {
-    console.log('Inside timeout, after 2000 seconds')
-}, 5000)
+// sleep(2000).then(() => console.log('After 2 sec'))
+// sleep(3000).then(() => console.log('After 3 sec'))
 
-setTimeout(timeout2sec, 2000)
+Promise.all([sleep(2000), sleep(5000)]).then(() => {
+        console.log('All promises')
+    })
 
-console.log('End')
+Promise.race([sleep(2000), sleep(5000)]).then(() => {
+    console.log('Race promises')
+})
