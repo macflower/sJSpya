bec0a318f0ac09e62a6fa1682d4b84ed215b7d39
macflower <stonemm21074mm@gmail.com>
2022-01-01 16:48:55 +0500

    commit Class

--- index.js
+++ index.js
@@ -1,38 +1,104 @@
-// Object
-const person = Object.create(
-    {
-        calculateAge() {
-            console.log('Age: ', new Date().getFullYear() - this.birthYear)
-        }
-    }, {
-    name: {
-        value: 'Vladilen',
-        enumerable: true,
-        writable: true,
-        configurable: true
-    },
-    birthYear: {
-        value: 1993,
-        enumerable: false,
-        writable: false,
-        configurable: false
-    },
-    age: {
-        get() {
-            return new Date().getFullYear() - this.birthYear
-        },
-        set(value) {
-            document.body.style.background = 'red'
-            console.log('Set age', value)
-        }
+// // const animal = {
+// //     name: 'Animal',
+// //     age: 5,
+// //     hasTail: true
+// // }
+//
+// class Animal {
+//     static type = 'ANIMAL'
+//
+//     constructor(options) {
+//         this.name = options.name
+//         this.age = options.age
+//         this.hasTail = options.hasTail
+//     }
+//
+//     voice() {
+//         console.log('I am Animal!')
+//     }
+// }
+//
+//
+// // const animal = new Animal({
+// //     name: 'Animal',
+// //     age: 5,
+// //     hasTail: true
+// // })
+//
+// class Cat extends Animal {
+//     static type = 'CAT'
+//
+//     constructor(options) {
+//         super(options)
+//         this.color = options.color
+//     }
+//
+//     voice() {
+//         super.voice()
+//         console.log('I am cat')
+//     }
+//
+//     get ageInfo() {
+//         return this.age * 7
+//     }
+//
+//     set ageInfo(newAge) {
+//         this.age = newAge
+//     }
+// }
+//
+// const cat = new Cat ({
+//     name: 'Cat',
+//     age: 7,
+//     hasTail: true,
+//     color: 'black'
+// })
+
+class Component {
+    constructor(selector) {
+        this.$el = document.querySelector(selector)
     }
+
+    hide() {
+        this.$el.style.display = 'none'
+    }
+
+    show() {
+        this.$el.style.display = 'block'
+    }
+}
+
+class Box extends Component {
+    constructor(options) {
+        super(options.selector);
+
+        this.$el.style.width = this.$el.style.height = options.size + 'px'
+        this.$el.style.background = options.color
+    }
+}
+
+const box1 = new Box({
+    selector: '#box1',
+    size: 100,
+    color: 'red'
 })
 
-// person.name = 'Maxim'
+const box2 = new Box({
+    selector: '#box2',
+    size: 120,
+    color: 'blue'
+})
 
-for (let key in person) {
-    if (person.hasOwnProperty(key)) {
-        console.log('Key', key, person[key])
+class Circle extends Box {
+    constructor(options) {
+        super(options);
+
+        this.$el.style.borderRadius = '50%'
     }
-
 }
+
+const c = new Circle({
+    selector: '#circle',
+    size: 90,
+    color: 'green'
+})
